edd724190381715f531d6a55f1a170db3ff0a000
giorgigigauri <ggg.gigauri@gmail.com>
2023-04-01 14:59:47 +0400

    hotfix

--- config/tbcbank.php
+++ config/tbcbank.php
@@ -2,9 +2,9 @@
 
 // config for Giorgigigauri/Tbcbank
 return [
-    'client_id' => env('TBC_CLIENT_ID', '5399999'),
-    'client_secret' => env('TBC_CLIENT_SECRET', 'f61a48'),
-    'api_key' => env('TBC_API_KEY', 'wN5nRcjd61V8raj5OfCE1mRhG6kkbaeI'),
-    'api_url' => env('TBC_API_URL', 'https://api.tbcbank.ge/v1/tpay/'),
+    'client_id' => env('TBC_CLIENT_ID', ''),
+    'client_secret' => env('TBC_CLIENT_SECRET', ''),
+    'api_key' => env('TBC_API_KEY', ''),
+    'api_url' => env('TBC_API_URL', ''),
     'callback_url' => env('TBC_CALLBACK_URL', ''),
 ];
